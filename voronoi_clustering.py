0a49b286c44e890119a217f06f44cccf3c2d6af1
Arian Arab <68286434+arian-arab@users.noreply.github.com>
2024-12-24 13:18:25 -0500

    Update voronoi_clustering.py

--- voronoi_clustering.py
+++ voronoi_clustering.py
@@ -10,14 +10,14 @@ from matplotlib import cm
 
 number_of_points = 1000
 area_threshold = 40
-min_number_of_localizations = 5
+min_number_of_localizations = 10
 
 def PolyArea(x,y):
     return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
 
 def neighbors_delauay(points):
     dt = Delaunay(points)
-    vertices = dt.vertices
+    vertices = dt.simplices
     
     vertices_hstack = np.hstack(vertices)
     vertices_hstack_sorted = np.sort(vertices_hstack) 
@@ -86,16 +86,16 @@ def plot_clusters(clusters_voronoi_cells,clusters_voronoi_areas):
         color.append([counter]*len(i))
         for j in i:
             patches.append(Polygon(j))
-    color = np.asarray([np.asarray(i) for i in color])
+    color = [np.asarray(i) for i in color]
     color = np.hstack(color)
     collection = PatchCollection(patches, alpha = 1, edgecolor = None, cmap=cm.hsv)
     collection.set_array(color)
-    # collection.set_clim(cmin, cmax)
     ax.add_collection(collection)
-    fig.colorbar(collection, ax=ax)
-    plt.show()
+    fig.colorbar(collection, ax=ax)    
     plt.xlim(min(points[:,0]),max(points[:,0]))
     plt.ylim(min(points[:,1]),max(points[:,1]))
+    plt.show()
+
 
 def plot_voronoi(points,voronoi_cells,voronoi_areas,keep_points):
     fig, ax = plt.subplots()
@@ -107,10 +107,11 @@ def plot_voronoi(points,voronoi_cells,voronoi_areas,keep_points):
     color = [i for i in voronoi_areas if i!=math.inf]
     collection.set_array(np.log(np.array(color)))
     ax.add_collection(collection)    
-    plt.scatter(points[:,0], points[:,1], s=1, color = 'b')
-    plt.show()
+    plt.scatter(points[:,0], points[:,1], s=1, color = 'b')    
+    plt.axis('equal')
     plt.xlim(min(points[:,0]),max(points[:,0]))
-    plt.ylim(min(points[:,1]),max(points[:,1]))
+    plt.ylim(min(points[:,1]),max(points[:,1]))    
+    plt.show()
     
     voronoi_cells = list(compress(voronoi_cells,keep_points))
     voronoi_areas = list(compress(voronoi_areas,keep_points))
@@ -123,10 +124,11 @@ def plot_voronoi(points,voronoi_cells,voronoi_areas,keep_points):
     color = [i for i in voronoi_areas if i!=math.inf]
     collection.set_array(np.log(np.array(color)))
     ax.add_collection(collection)    
-    plt.scatter(points[:,0], points[:,1], s=1, color = 'b')
-    plt.show()
+    plt.scatter(points[:,0], points[:,1], s=1, color = 'b')  
+    plt.axis('equal')
     plt.xlim(min(points[:,0]),max(points[:,0]))
-    plt.ylim(min(points[:,1]),max(points[:,1]))
+    plt.ylim(min(points[:,1]),max(points[:,1]))    
+    plt.show()
     
 points = np.random.random([number_of_points,2])
 neighbors = neighbors_delauay(points)
